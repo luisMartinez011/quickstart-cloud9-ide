c3092cb5c79fb2b644e53656f9bb2182b9bb2f76
Jay McConnell <jmmccon@amazon.com>
2019-09-30 12:34:41 -0700

    split custom resources per function

---
+++ functions/source/c9DiskResize/lambda_function.py
@@ -0,0 +1,91 @@
+from __future__ import print_function
+import logging
+from time import sleep
+import boto3
+from crhelper import CfnResource
+
+logger = logging.getLogger(__name__)
+helper = CfnResource(json_logging=True, log_level='DEBUG', boto_level='CRITICAL')
+
+try:
+    ssm_client = boto3.client('ssm')
+    ec2_client = boto3.client('ec2')
+except Exception as e:
+    helper.init_failure(e)
+
+
+def get_command_output(instance_id, command_id):
+    response = ssm_client.get_command_invocation(CommandId=command_id, InstanceId=instance_id)
+    if response['Status'] in ['Pending', 'InProgress', 'Delayed']:
+        return
+    return response
+
+
+def send_command(instance_id, commands):
+    logger.debug("Sending command to %s : %s" % (instance_id, commands))
+    try:
+        return ssm_client.send_command(InstanceIds=[instance_id], DocumentName='AWS-RunShellScript',
+                                       Parameters={'commands': commands})
+    except ssm_client.exceptions.InvalidInstanceId:
+        logger.debug("Failed to execute SSM command", exc_info=True)
+        return
+
+
+@helper.create
+def create(event, context):
+    logger.debug("Got Create")
+    instance_id = event["ResourceProperties"]["InstanceId"]
+    size = event['ResourceProperties']['Size']
+
+    ### TODO: Do EBS volume resize
+
+
+@helper.poll_create
+def poll_create(event, context):
+    logger.info("Got create poll")
+    instance_id = event["ResourceProperties"]["InstanceId"]
+    region = event['ResourceProperties']['Region']
+    size = event['ResourceProperties']['Size']
+
+    # TODO: check if resize is complete, return if not, eg:
+    #  if ec2_client.describe_volume(Id=my_vol_id)["VolumeId"] != "COMPLETE":
+    #      return
+
+    # executing OS filesystem resize
+    while True:
+        commands = ['mkdir -p /tmp/setup', 'cd /tmp/setup',
+                    'aws configure set region ' + region,
+                    ### TODO: OS Filesystem resize command here
+                    ]
+        send_response = send_command(instance_id, commands)
+        if send_response:
+            command_id = send_response['Command']['CommandId']
+            break
+        if context.get_remaining_time_in_millis() < 20000:
+            raise Exception("Timed out attempting to send command to SSM")
+        sleep(15)
+
+    # Wait for resize to complete
+    while True:
+        try:
+            cmd_output_response = get_command_output(instance_id, command_id)
+            if cmd_output_response:
+                break
+        except ssm_client.exceptions.InvocationDoesNotExist:
+            logger.debug('Invocation not available in SSM yet', exc_info=True)
+        if context.get_remaining_time_in_millis() < 20000:
+            raise Exception("Timed out waiting for filesystem to be resized")
+        sleep(15)
+    if cmd_output_response['StandardErrorContent']:
+        raise Exception("ssm command failed: " + cmd_output_response['StandardErrorContent'][:235])
+    return instance_id
+
+
+@helper.update
+@helper.delete
+def no_op(_, __):
+    return
+
+
+def handler(event, context):
+    helper(event, context)
--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,18 +1,3 @@
 #!/bin/bash
 
-# Specify the desired volume size in GiB as a command-line argument. If not specified, default to 20 GiB.
-SIZE=${1}
-
-# Install the jq command-line JSON processor.
-sudo yum install -y jq 2&> /dev/null
-
-# Get the ID of the envrionment host Amazon EC2 instance.
-INSTANCE_ID=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
-
-# Get the ID of the Amazon EBS volume associated with the instance.
-VOLUMEID=$(aws ec2 describe-instances \
-  --instance-id $INSTANCEID | \
-  jq -r .Reservations[0].Instances[0].BlockDeviceMappings[0].Ebs.VolumeId)
-
-# Resize the EBS volume.
-aws ec2 modify-volume --volume-id $VOLUMEID --size $SIZE
+# placeholder for custom bootstrap file
